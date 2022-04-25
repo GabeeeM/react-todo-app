9b1690f7cd6e55f70b76cf5521a9d543121a6749
Gabriel Mack <69842642+GabeeeM@users.noreply.github.com>
2022-04-25 11:34:02 -0700

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+  const [items, setItems] = useState();
+
+  const addTask = (e) => {
+    e.preventDefault();
+    setList(list.concat(input));
+    console.log(list);
+
+    setItems(
+      list.map((x) => (
+        <div>
+          <li key={x}>{x}</li>
+        </div>
+      ))
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="bg-amber-600 h-screen flex flex-col justify-center items-center">
+        <h1>Todo App</h1>
+        <form onSubmit={(e) => addTask(e)}>
+          <input
+            type="text"
+            name="name"
+            onChange={(x) => setInput(x.target.value)}
+            placeholder="Add a task"
+          ></input>
+        </form>
+        <ul>{items}</ul>
+      </div>
     </div>
   );
 }
