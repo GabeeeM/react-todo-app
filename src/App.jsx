d68d5ff6449c087ef11a8b90bd0ff6ad0034a45d
Gabriel Mack <69842642+GabeeeM@users.noreply.github.com>
2022-04-26 10:35:47 -0700

    progress
    fixed the weird state updates whenever you submit the form, and added a little styling with a checkbox on each list element

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,43 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [list, setList] = useState([]);
   const [input, setInput] = useState("");
   const [items, setItems] = useState();
+  const [list, setList] = useState([]);
 
   const addTask = (e) => {
-    e.preventDefault();
-    setList(list.concat(input));
-    console.log(list);
+    let tempList = list.concat(input);
+    setList((list) => list.concat(input));
+    console.log(tempList);
 
     setItems(
-      list.map((x) => (
+      tempList.map((x) => (
         <div>
-          <li key={x}>{x}</li>
+          <div className="bg-white p-[1rem] sm:w-[40rem] flex flex-row gap-[1rem]">
+            <input type="checkbox" onClick={() => completeTask(x)} />
+            <li key={x}>{x}</li>
+          </div>
+          <hr />
         </div>
       ))
+    );
+
+    setInput("");
+
+    e.preventDefault();
+  };
+
+  const completeTask = (task) => {
+    return (
+      <div>
+        <div className="bg-white p-[1rem] sm:w-[40rem] flex flex-row gap-[1rem]">
+          <s>
+            <input type="checkbox" onClick={() => completeTask(task)} />
+            <li key={task}>{task}</li>
+          </s>
+        </div>
+        <hr />
+      </div>
     );
   };
 
@@ -28,11 +50,14 @@ function App() {
           <input
             type="text"
             name="name"
+            value={input}
             onChange={(x) => setInput(x.target.value)}
             placeholder="Add a task"
           ></input>
         </form>
-        <ul>{items}</ul>
+        <div className="rounded-sm">
+          <ul>{items}</ul>
+        </div>
       </div>
     </div>
   );
